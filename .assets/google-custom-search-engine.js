4676d78fc265d51beefedbd125287d0bcfa2d4f4
TW Yang <twy@30abysses.com>
2016-11-20 17:22:45 -0800

    Generated by update-website

---
+++ .assets/google-custom-search-engine.js
@@ -0,0 +1,9 @@
+  (function() {
+    var cx = '001912921156749988317:usxfccd2upi';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
